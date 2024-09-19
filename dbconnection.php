0707f1da98b388112fb25abe5fbb43a481f053f3
shawn owiro <135601397+shawnowiro@users.noreply.github.com>
2024-09-19 22:11:36 +0300

    created insertion logic

--- dbconnection.php
+++ dbconnection.php
@@ -4,35 +4,41 @@ $servername = "localhost";
 $username = "username";
 $password = "password";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+class DbConnection{
+    private $servername;
+    private $username;
+    private $password;
+    private static $instance = null;
+    private $connection;
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-    class DbConnection{
-        private $servername;
-        private $username;
-        private $password;
-        private static $instance = null;
-
-        private function __construct($servername,$username,$password){
-            $this->servername = $servername;
-            $this->username = $username;
-            $this->password = $password;
+    private function __construct($servername,$username,$password){
+        $this->servername = $servername;
+        $this->username = $username;
+        $this->password = $password;
+        $this->connection = new mysqli($servername, $username, $password);
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
         }
-
-        public static function getInstance($servername,$username,$password){
-            if (!DbConnection::$instance) {
-                DbConnection::$instance = new DbConnection($servername,$username,$password);
-            }
-            return DbConnection::$instance;
-        }
-        PUBLIC static function insert(){
-            
-        }
+        echo "Connected successfully";
     }
-?>
+
+    public static function getInstance($servername,$username,$password){
+        if (!DbConnection::$instance) {
+            DbConnection::$instance = new DbConnection($servername,$username,$password);
+        }
+        return DbConnection::$instance;
+    }
+
+    public function insert($First_name, $Last_name,$Phone_number, $Email,$password){
+        $sql = "INSERT INTO users (First_name, Last_name,Phone_number, Email,password) VALUES (?, ?, ?,?)";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bind_param("sssss", $First_name, $Last_name,$Phone_number,$Email,$password);
+        
+        if ($stmt->execute()) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $stmt->error;
+        }
+        $stmt->close();
+    }
+}
