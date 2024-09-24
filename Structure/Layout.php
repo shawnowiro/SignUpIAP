329ca2b7df1db4d14b91545dee079ca52cd4275b
shawn owiro <135601397+shawnowiro@users.noreply.github.com>
2024-09-24 15:44:30 +0300

    removed tailwind

--- Structure/Layout.php
+++ Structure/Layout.php
@@ -11,7 +11,7 @@ class Layout
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-            <script src="https://cdn.tailwindcss.com"></script>
+
             <link href="Css/style.css" rel="stylesheet">
             <title>Sign Up</title>
         </head>
@@ -19,7 +19,7 @@ class Layout
         <body>
             <nav class="navbar bg-body-tertiary">
                 <form class="container-fluid justify-content-start">
-                    <button class="btn btn-outline-success me-2" type="button">Main button</button>
+                    <button class="btn btn-outline-success me-2 bg-red" type="button">Main button</button>
                     <button class="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
                 </form>
             </nav>
