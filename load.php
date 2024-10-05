9e8465b5abc6c11876e3864f01d39f924d66c563
shawn owiro <135601397+shawnowiro@users.noreply.github.com>
2024-10-05 17:29:28 +0300

    added php mailer

--- load.php
+++ load.php
@@ -1,7 +1,10 @@
 <?php
     require('Structure/Layout.php');
     require('form.php');
-
+    require('Dbconnection/dbconnection.php');
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
     $ObjLayouts = new Layout();
     $ObjForms = new forms();
 
